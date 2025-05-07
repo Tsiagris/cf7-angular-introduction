3d379121a505978fa8b15b2c5a63e13b87ccbc05
Anastassios Siagkris <asiagris2011@gmail.com>
2025-05-07 19:16:10 +0300

    current person added

--- src/app/components/eperson-template-driven-form/eperson-template-driven-form.component.ts
+++ src/app/components/eperson-template-driven-form/eperson-template-driven-form.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {FormsModule, NgForm} from '@angular/forms'
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field'; 
 import { MatButtonModule } from '@angular/material/button';
+import { EPerson } from 'src/app/shared/interfaces/eperson';
+import { EventBindExampleComponent } from '../event-bind-example/event-bind-example.component';
 
 
 @Component({
   selector: 'app-eperson-template-driven-form',
   imports: [
+    CommonModule,
     FormsModule,
     MatSelectModule,
     MatInputModule,
@@ -19,5 +23,27 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './eperson-template-driven-form.component.css'
 })
 export class EpersonTemplateDrivenFormComponent {
+  @Output() person = new EventEmitter<EPerson>()
+  @ViewChild('eForm', {static:false}) form:NgForm | undefined;
+  
+  onSubmit(value: EPerson){
+    console.log(value);
+    console.log(this.form);
+    console.log(this.form?.form.get('givenName')?.value)
+    console.log(this.form?.form.controls['surName'].value)
+    this.person.emit(value);
+  }
+
+  onSetValue() {
+    this.form?.setValue({
+      givenName:"Jhon",
+      surName:"Doe",
+      age: 30,
+      email:"john@aueb.gr",
+      education:"Bachelor's degree"
+
+    });
+    this.form?.controls['givenName'].setValue("aaaa")
+  }
 
 }
--- src/app/components/person-table/person-table.component.ts
+++ src/app/components/person-table/person-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { EPerson } from 'src/app/shared/interfaces/eperson';
 import { Person } from 'src/app/shared/interfaces/person';
 
 @Component({
@@ -8,8 +9,9 @@ import { Person } from 'src/app/shared/interfaces/person';
   styleUrl: './person-table.component.css'
 })
 export class PersonTableComponent {
-  @Input() personInput: Person | undefined;
+  @Input() personInput: Person | EPerson | undefined;
   name = "Tassos"
+  addressOReducation: string = ''
 
   person = {
     givenName:"Tassos",
@@ -17,5 +19,19 @@ export class PersonTableComponent {
     age:40,
     email:"asiagris2011@gmail.com"
   }
+  isPerson(): boolean {
+    if(this.personInput && 'address' in this.personInput) {
+      this.addressOReducation = this.personInput.address
+      return 'address' in this.personInput;
+    }
+  return false;
+  }
 
+  isEPerson(): boolean {
+    if (this.personInput && 'education' in this.personInput) {
+      this.addressOReducation = this.personInput.education
+      return 'education' in this.personInput;
+    }
+    return false;
+  }
 }
